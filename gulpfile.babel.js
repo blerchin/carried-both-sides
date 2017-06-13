54f381ef3f108dc1124d0e476c28c3957dd1249b
Ben Lerchin <ben@lerchin.com>
2017-06-13 01:01:09 -0700

    full webpack, no live reload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,10 @@
 import gulp from "gulp";
 import cp from "child_process";
 import gutil from "gulp-util";
-import BrowserSync from "browser-sync";
 import webpack from "webpack";
-import webpackStream from "webpack-stream";
+import WebpackDevServer from "webpack-dev-server";
 import webpackConfig from "./webpack.conf";
 
-const browserSync = BrowserSync.create();
 const hugoBin = "hugo";
 const defaultArgs = ["-d", "../.tmp", "-s", "site", "-v"];
 
@@ -16,54 +14,48 @@ gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture
 
 gulp.task("build", ["hugo", "webpack"]);
 
-gulp.task("webpack", ["hugo"], () => {
-  return gulp.src("./.tmp/**/*.html")
-    .pipe(webpackStream(webpackConfig, webpack))
-    .pipe(gulp.dest("./dist"));
+gulp.task("webpack", ["hugo"], (cb) => {
+  webpack(webpackConfig, (err) => {
+    if (err) throw new gutil.PluginError("webpack", err);
+    cb();
+  });
 });
 
 gulp.task("webpack-watch", ["hugo"], (cb) => {
   const config = Object.assign({
-    watch: true,
-    watchOptions: {
-      aggregateTimeout: 500,
-    }
+    watch: true
   }, webpackConfig);
-  let init = false;
-  gulp.src("./.tmp/**/*.html")
-    .pipe(webpackStream(config, webpack, (err, stats) => {
-      if (err) throw new gutil.PluginError("webpack", err);
-      gutil.log("[webpack]", stats.toString({
-        colors: true,
-        progress: true
-      }));
-      if (!init) {
-        cb();
-        init = true;
-      }
-      browserSync.reload();
-    }))
-    .pipe(gulp.dest("./dist"));
+  const compiler = webpack(config);
+  new WebpackDevServer(compiler, {
+    stats: {
+      colors: true,
+      progress: true
+    }
+  }).listen(3000, "localhost", (err) => {
+    if (err) throw new gutil.PluginError("webpack-dev-server", err);
+    // Server listening
+    /*
+    gutil.log("[webpack]", stats.toString({
+      colors: true,
+      progress: true
+    }));
+    */
+  });
 });
 
-gulp.task("server", ["hugo", "webpack-watch"], () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
+gulp.task("hugo-watch", () => {
   gulp.watch("./site/**/*", ["hugo"]);
 });
+
+gulp.task("server", ["hugo-watch", "webpack-watch"]);
 
 function buildSite(cb, options) {
   const args = options ? defaultArgs.concat(options) : defaultArgs;
 
   return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
     if (code === 0) {
-      browserSync.reload();
       cb();
     } else {
-      browserSync.notify("Hugo build failed :(");
       cb("Hugo build failed");
     }
   });
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,4 +1,5 @@
 import webpack from "webpack";
+import glob from "glob";
 import path from "path";
 import postCSS from "./config/postcss";
 
@@ -14,7 +15,7 @@ export default {
           {
             loader: "html-loader",
             options: {
-              attrs: ["img:src", "link:href", "script:src"],
+              attrs: ["img:src", "script:src", "link:href"],
               interpolate: "require"
             }
           }
@@ -29,7 +30,6 @@ export default {
       {
         test: /\.(jpe?g|png|gif|tiff)$/i,
         loaders: [
-          "file-loader?name=img/[hash].[ext]",
           {
             loader: "image-webpack-loader",
             options: {
@@ -42,11 +42,11 @@ export default {
       {
         test: /.(css|sass|scss)$/i,
         use: [
-          "file-loader?name=css/[name].[hash:4].css",
+          "file-loader?name=css/[hash].css",
           "extricate-loader?resolve=\\.js$",
           "css-loader",
           {loader: "postcss-loader", options: postCSS},
-          "sass-loader?outputStyle=nested"
+          "sass-loader"
         ]
       },
       {test: /\.json$/, loader: "json-loader"},
@@ -55,21 +55,28 @@ export default {
         exclude: /node_modules/,
         loaders: [
           "entry-loader?name=js/[name].js",
-          {
-            loader: "babel-loader",
-            options: {
-              cacheDirectory: true
-            }
-          }
+          "babel-loader"
         ],
       }
     ]
+  },
+  entry: function() {
+    return glob.sync("*.html", {
+      absolute: true, // Receive absolute paths for matched files.
+      cwd: this.context, // The current working directory in which to search.
+      matchBase: true, // Perform a basename-only match.
+      nodir: true, // Do not match directories, only files.
+      nosort: true // Don't sort the results.
+    });
+  },
+  output: {
+    path: path.join(__dirname, "./dist")
   },
   plugins: [
     new webpack.ProvidePlugin({
       "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
     }),
-    new webpack.EnvironmentPlugin({NODE_ENV: "development"})
+    new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
     modules: [
